bd63efb42acaab0939c7a44452816e93c7c78b5d
Chris Ertel <crertel@crockbox>
2014-07-12 00:51:22 -0500

    Add stub API routes.
    Add route for adding markers.
    Add route for querying markers.
    Add route for creating units.
    Add route for updating units.
    Add route for getting units.
    Add vim swap to git ignore.

--- app.js
+++ app.js
@@ -4,12 +4,50 @@ var express = require("express");
 var log4js = require("log4js");
 var sockjs = require("sockjs")
 var log = log4js.getLogger("backend");
+ 
+function getUnitsForGroup( req, res ) {
+    var groupId = req.params.groupId;
+    log.info("Getting units for group %s...", groupId);
+    res.status(200).send();
+};
+
+function getMarkersForGroup( req, res ) {
+    var groupId = req.params.groupId;
+    log.info("Getting units for group %s...", groupId);
+    res.status(200).send();
+};
+
+function addUnitToGroup( req, res ) {
+    var groupId = req.params.groupId;
+    log.info("Getting units for group %s...", groupId);
+    res.status(201).send();
+};
+
+function addMarkerToGroup( req, res ) {
+    var groupId = req.params.groupId;
+    log.info("Getting units for group %s...", groupId);
+    res.status(201).send();
+};
+
+function updateUnit( res, req ) {
+    var groupId = req.params.groupId;
+    var unitId = req.params.unitId;
+    log.info("Updating unit %s for group %s...", unitId, groupId);
+    res.status(201).send();
+};
 
 function startWeb() {
     var port = 3000;
     log.info("Starting web server on port %s...", port);
     var server = express();
+
+    server.get( "/api/1.0.0/groups/:groupId/units", getUnitsForGroup );
+    server.get( "/api/1.0.0/groups/:groupId/markers", getMarkersForGroup );
+    server.post( "/api/1.0.0/groups/:groupId/markers", addMarkerToGroup );
+    server.post( "/api/1.0.0/groups/:groupId/units", addUnitToGroup );
+    server.put( "/api/1.0.0/groups/:groupId/units/:unitId", updateUnit );
     server.use( "/", express.static(__dirname + "/web"));
+    
     var webserver = server.listen(port);
     log.info("done.");
 };
